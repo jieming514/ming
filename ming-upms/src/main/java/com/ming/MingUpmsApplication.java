261c0be2cb392cf604bb9c1c095ef46210cfa811
jie_ming514 <jie_ming514@163.com>
2020-04-30 22:50:44 +0800

    --  包更新 by  jie_ming514

--- ming-upms/src/main/java/com/ming/MingUpmsApplication.java
+++ ming-upms/src/main/java/com/ming/MingUpmsApplication.java
@@ -1,12 +1,8 @@
 package com.ming;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
