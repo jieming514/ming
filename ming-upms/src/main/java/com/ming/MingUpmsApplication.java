e50c099e1df3d586649dc411e6797c61246f152e
jie_ming514 <jie_ming514@163.com>
2020-05-13 20:18:58 +0800

    --mybatis使用别名方式 work by jie_ming514

--- ming-upms/src/main/java/com/ming/MingUpmsApplication.java
+++ ming-upms/src/main/java/com/ming/MingUpmsApplication.java
@@ -3,7 +3,9 @@ package com.ming;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class MingUpmsApplication {
 
