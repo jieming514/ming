6be4129c4498b0c7d6e6c70fff023a1d77e175c9
jie_ming514 <jie_ming514@163.com>
2020-05-13 22:20:05 +0800

    --调整排序  work by jie_ming514

--- ming-upms/src/main/resources/static/js/appjs/system/upmsRole/upmsRole.js
+++ ming-upms/src/main/resources/static/js/appjs/system/upmsRole/upmsRole.js
@@ -10,9 +10,9 @@ function load() {
 					{
 						method : 'get', // 服务器数据的请求方式 get or post
 						url : prefix + "/list", // 服务器数据的加载地址
-					//	showRefresh : true,
-					//	showToggle : true,
-					//	showColumns : true,
+						showRefresh : true,
+						showToggle : true,
+						showColumns : true,
 						iconSize : 'outline',
 						toolbar : '#exampleToolbar',
 						striped : true, // 设置为true会有隔行变色效果
@@ -26,7 +26,6 @@ function load() {
 						pageSize : 10, // 如果设置了分页，每页数据条数
 						pageNumber : 1, // 如果设置了分布，首页页码
 						//search : true, // 是否显示搜索框
-						showColumns : false, // 是否显示内容下拉框（选择显示的列）
 						sidePagination : "server", // 设置在哪里进行分页，可选值为"client" 或者 "server"
 						queryParams : function(params) {
 							return {
--- ming-upms/src/main/resources/static/js/appjs/system/upmsSystem/upmsSystem.js
+++ ming-upms/src/main/resources/static/js/appjs/system/upmsSystem/upmsSystem.js
@@ -10,9 +10,9 @@ function load() {
 					{
 						method : 'get', // 服务器数据的请求方式 get or post
 						url : prefix + "/list", // 服务器数据的加载地址
-					//	showRefresh : true,
-					//	showToggle : true,
-					//	showColumns : true,
+						showRefresh : true,
+						showToggle : true,
+						showColumns : true,
 						iconSize : 'outline',
 						toolbar : '#exampleToolbar',
 						striped : true, // 设置为true会有隔行变色效果
@@ -26,13 +26,16 @@ function load() {
 						pageSize : 10, // 如果设置了分页，每页数据条数
 						pageNumber : 1, // 如果设置了分布，首页页码
 						//search : true, // 是否显示搜索框
-						showColumns : false, // 是否显示内容下拉框（选择显示的列）
 						sidePagination : "server", // 设置在哪里进行分页，可选值为"client" 或者 "server"
+						sortable : true,
+						sortOrder: "asc",
 						queryParams : function(params) {
 							return {
 								//说明：传入后台的参数包括offset开始索引，limit步长，sort排序列，order：desc或者,以及所有列的键值对
 								limit: params.limit,
-								offset:params.offset
+								offset:params.offset,
+                                order : params.order,
+                                sort : params.sort
 					           // name:$('#searchName').val(),
 					           // username:$('#searchName').val()
 							};
@@ -48,12 +51,13 @@ function load() {
 									checkbox : true
 								},
 																{
-									field : 'systemId', 
-									title : '编号' 
+									field : 'systemId',
+									title : '编号'
 								},
 								{
                                     field : 'name',
-                                    title : '系统名称'
+                                    title : '系统名称',
+                                    sortable: true
                                 },
                                                                 {
                                     field : 'title',
@@ -93,7 +97,8 @@ function load() {
 								},
 																{
 									field : 'ctime', 
-									title : '创建时间' 
+									title : '创建时间' ,
+									sortable: true
 								},
 																{
 									field : 'orders', 
