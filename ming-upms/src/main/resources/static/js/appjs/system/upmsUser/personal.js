63cf9273b7a25946c3572c912e5c339524a2cd2c
jie_ming514 <jie_ming514@163.com>
2020-05-23 15:04:50 +0800

    --更新个人用户界面 work by jie_ming514

--- ming-upms/src/main/resources/static/js/appjs/system/upmsUser/personal.js
+++ ming-upms/src/main/resources/static/js/appjs/system/upmsUser/personal.js
@@ -1,62 +1,127 @@
-var prefix = "/system/upmsUser"
-$(function () {
+/*用户管理-头像*/
+function avatar() {
+    var url = 'system/user/profile/avatar';
+    $.modal.open("修改头像", url);
+}
 
-});
-/**
- * 基本信息提交
- */
-$("#base_save").click(function () {
-    var hobbyStr = getHobbyStr();
-    $("#hobby").val(hobbyStr);
-    if($("#basicInfoForm").valid()){
-            $.ajax({
-                cache : true,
-                type : "POST",
-                url :"/sys/user/updatePeronal",
-                data : $('#basicInfoForm').serialize(),
-                async : false,
-                error : function(request) {
-                    laryer.alert("Connection error");
-                },
-                success : function(data) {
-                    if (data.code == 0) {
-                        parent.layer.msg("更新成功");
-                    } else {
-                        parent.layer.alert(data.msg)
+/*用户信息-修改*/
+$("#form-user-edit").validate({
+    onkeyup: false,
+    rules:{
+        realname:{
+            required:true,
+        },
+        email:{
+            required:true,
+            email:true,
+            remote: {
+                url: "system/user/checkEmailUnique",
+                type: "post",
+                dataType: "json",
+                data: {
+                    "userId": function() {
+                        return $("#userId").val();
+                    },
+                    "email": function() {
+                        return $.common.trim($("#email").val());
                     }
-                }
-            });
-        }
-
-});
-$("#pwd_save").click(function () {
-    if($("#modifyPwd").valid()){
-        $.ajax({
-            cache : true,
-            type : "POST",
-            url :"/sys/user/resetPwd",
-            data : $('#modifyPwd').serialize(),
-            async : false,
-            error : function(request) {
-                parent.laryer.alert("Connection error");
-            },
-            success : function(data) {
-                if (data.code == 0) {
-                    parent.layer.alert("更新密码成功");
-                    $("#photo_info").click();
-                } else {
-                    parent.layer.alert(data.msg)
+                },
+                dataFilter: function (data, type) {
+                    return $.validate.unique(data);
                 }
             }
-        });
-    }
-});
-function getHobbyStr(){
-    var hobbyStr ="";
-    $(".hobby").each(function () {
-        if($(this).is(":checked")){
-            hobbyStr+=$(this).val()+";";
+        },
+        phonenumber:{
+            required:true,
+            isPhone:true,
+            remote: {
+                url: "system/user/checkPhoneUnique",
+                type: "post",
+                dataType: "json",
+                data: {
+                    "userId": function() {
+                        return $("#userId").val();
+                    },
+                    "phonenumber": function() {
+                        return $.common.trim($("#phonenumber").val());
+                    }
+                },
+                dataFilter: function (data, type) {
+                    return $.validate.unique(data);
+                }
+            }
+        },
+    },
+    messages: {
+        "realname": {
+            required: "请输入用户名称",
+        },
+        "email": {
+            required: "请输入邮箱",
+            remote: "Email已经存在"
+        },
+        "phonenumber":{
+            required: "请输入手机号码",
+            remote: "手机号码已经存在"
         }
-    });
-   return hobbyStr;
+    },
+    focusCleanup: true
+});
+
+function submitUserInfo() {
+    if ($.validate.form()) {
+        $.operate.saveModal(ctx + "system/user/profile/update", $('#form-user-edit').serialize());
+    }
+}
+
+/*用户管理-修改密码*/
+$("#form-user-resetPwd").validate({
+    onkeyup: false,
+    rules:{
+        oldPassword:{
+            required:true,
+            remote: {
+                url: "system/user/profile/checkPassword",
+                type: "get",
+                dataType: "json",
+                data: {
+                    password: function() {
+                        return $("input[name='oldPassword']").val();
+                    }
+                }
+            }
+        },
+        newPassword: {
+            required: true,
+            minlength: 6,
+            maxlength: 20
+        },
+        confirmPassword: {
+            required: true,
+            equalTo: "#newPassword"
+        }
+    },
+    messages: {
+        oldPassword: {
+            required: "请输入原密码",
+            remote: "原密码错误"
+        },
+        newPassword: {
+            required: "请输入新密码",
+            minlength: "密码不能小于6个字符",
+            maxlength: "密码不能大于20个字符"
+        },
+        confirmPassword: {
+            required: "请再次输入新密码",
+            equalTo: "两次密码输入不一致"
+        }
+
+    },
+    focusCleanup: true
+});
+
+function submitChangPassword () {
+    if ($.validate.form("form-user-resetPwd")) {
+        $.operate.saveModal(ctx + "system/user/profile/resetPwd", $('#form-user-resetPwd').serialize());
+    }
 }
