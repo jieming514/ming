4ddfaf81395f04aa6619d420dcba53aaf9853702
jie_ming514 <jie_ming514@163.com>
2020-05-28 23:22:33 +0800

    --添加防止XXs攻击 work by jie_ming514

---
+++ ming-upms/src/main/java/com/ming/upms/common/config/XssConfig.java
@@ -0,0 +1,30 @@
+package com.ming.upms.common.config;
+
+import com.ming.upms.common.filter.XssFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class XssConfig{
+
+	/**
+	 * xss过滤拦截器
+	 */
+	@Bean
+	public FilterRegistrationBean xssFilterRegistrationBean() {
+		FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+		filterRegistrationBean.setFilter(new XssFilter());
+		filterRegistrationBean.setOrder(1);
+		filterRegistrationBean.setEnabled(false);
+		filterRegistrationBean.addUrlPatterns("/*");
+		Map<String, String> initParameters = new HashMap<>();
+		initParameters.put("excludes", "/favicon.ico,/img/*,/js/*,/css/*");
+		initParameters.put("isIncludeRichText", "true");
+		filterRegistrationBean.setInitParameters(initParameters);
+		return filterRegistrationBean;
+	}
+}
