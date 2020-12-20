dc9cf8a62818082a8ee9bc413c7dd9d1f7fdadf2
jieming514 <jie_ming514@163.com>
2020-12-20 22:44:24 +0800

    --添加全屏显示功能 work by jie_ming514

--- ming-admin/src/main/resources/static/js/app.js
+++ ming-admin/src/main/resources/static/js/app.js
@@ -276,3 +276,17 @@ $(function () {
 function localStorageSupport() {
     return (('localStorage' in window) && window['localStorage'] !== null)
 }
+
+
+function fullPage() {
+	if ($.util.supportsFullScreen) {
+		if ($.util.isFullScreen()) {
+			$.util.cancelFullScreen();
+
+		} else {
+			$.util.requestFullScreen();
+		}
+	} else {
+		alert("当前浏览器不支持全屏 API，请更换至最新的 Chrome/Firefox/Safari 浏览器或通过 F11 快捷键进行操作。");
+	}
+}
